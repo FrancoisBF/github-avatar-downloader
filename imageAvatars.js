d6548e9513869d7e03babbf1a96f579c51080831
Francois <francoisbergeret.francois@gmail.com>
2017-06-06 05:04:54 +0000

    image

---
+++ imageAvatars.js
@@ -0,0 +1,9 @@
+module.exports = function(url, filePath) {
+  var request = require("request");
+  var fs = require("fs");
+
+  request.get(url).on('response', function(response) {
+
+    var fileType = response.headers['content-type'].split("/")[1];
+    this.pipe(fs.createWriteStream(filePath + "." + fileType));
+  });
